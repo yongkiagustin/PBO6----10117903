ea05cd96db3adea4f5e846d4cb45d868f46e082a
yongkiagustin <yongkiagustin92@gmail.com>
2018-09-23 15:38:04 +0700

    mencoba

---
+++ Pertemuan1/latihan2/src/latihan2/Latihan2.java
@@ -0,0 +1,32 @@
+/*
+ * nama : yongki agustin
+ * kelas : if6k
+ * nim : 10117903
+ * Todo : latihan kedua   
+*/
+package latihan2;
+
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi variable
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+
+        // Mengisi Nilai Variable
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+
+        // Menampilkan nilai
+        System.out.println("\nIsi variable nilai\t= " + nilaiInt);
+        System.out.println("Isi variable PHI\t= " + PHI);
+        System.out.println("Isi variable logika\t= " + nilaiLogika);
+        System.out.println("Isi variable Karakter\t= " + nilaiKarakter);
+    }
+}
