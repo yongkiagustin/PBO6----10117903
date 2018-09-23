c19263faac548c2cf2762c3eac9c3949d50920cf
yongkiagustin <yongkiagustin92@gmail.com>
2018-09-23 15:37:54 +0700

    mencoba

---
+++ Pertemuan1/latihan1/src/latihan1/Latihan1.java
@@ -0,0 +1,28 @@
+/*
+ * nama : yongki agustin
+ * kelas : if6k
+ * nim : 10117903
+ * Todo : latihan output   
+*/
+package latihan1;
+
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "================");
+        System.out.println("Nama\t\t: Yongki Agustin");
+        System.out.println("Kelas\t\t: IF 6K");
+        System.out.println("NIM\t\t: 10117903");
+        System.out.println("Alamat\t\t: Baleendah, Kab. Bandung");
+        System.out.println("No HP\t\t: 089656592052");
+        System.out.println("Tempat Lahir\t: Bandung");
+        System.out.println("Tanggal Lahir\t: 16 Agustus 2018");
+        
+        
+        
+    }
+    
+}
