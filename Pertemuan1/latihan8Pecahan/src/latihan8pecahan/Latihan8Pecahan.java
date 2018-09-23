48c4a24f25c73d8827828eb771756a1cd0a449be
yongkiagustin <yongkiagustin92@gmail.com>
2018-09-23 15:49:53 +0700

    mencoba

---
+++ Pertemuan1/latihan8Pecahan/src/latihan8pecahan/Latihan8Pecahan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan8pecahan;
+
+/**
+ *
+ * @author Teknisi
+ */
+public class Latihan8Pecahan {
+
+    public static void main(String[] args) {
+
+        float vFloat;
+        double vDouble;
+
+        vFloat = 1234567890.0987654321F;
+        vDouble = 1_234_567_890.123456789;
+
+        System.out.printf("Nilai Float : %f\r\n", vFloat);
+        System.out.printf("Nilai Double : %f\r\n", vDouble);
+
+    }
+
+}
