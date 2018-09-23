c4f60b09c91ebfd63f73fdf3a9d48ef6a137d528
yongkiagustin <yongkiagustin92@gmail.com>
2018-09-23 15:57:26 +0700

    mencoba

---
+++ Pertemuan1/latihan12Aritmatika/src/latihan12aritmatika/Latihan12Aritmatika.java
@@ -0,0 +1,32 @@
+/*
+ * nama : yongki agustin
+ * kelas : if6k
+ * nim : 10117903
+ * Todo : latihan Aritmatika
+ */
+package latihan12aritmatika;
+
+
+public class Latihan12Aritmatika {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 25;
+        int c = 15;
+        int d = 20;
+
+        System.out.printf("a + b = %d\r\n", (a + b));
+        System.out.printf("a - b = %d\r\n", (a - b));
+        System.out.printf("a * b = %d\r\n", (a * b));
+        System.out.printf("b / a = %d\r\n", (b / a));
+        System.out.printf("b %% a = %d\r\n", (b % a));
+        System.out.printf("c %% a = %d\r\n", (c % a));
+
+        System.out.printf("a++ = %d\r\n", (a++));
+        System.out.printf("b-- = %d\r\n", (a--));
+        System.out.printf("d++ = %d\r\n", (d++));
+        System.out.printf("++d = %d\r\n", (++d));
+
+    }
+}
