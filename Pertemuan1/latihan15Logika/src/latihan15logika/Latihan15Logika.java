89ca50b42f001a4c583df0b8d2da7cda7a14a6f9
yongkiagustin <yongkiagustin92@gmail.com>
2018-09-23 16:05:41 +0700

    mencoba

---
+++ Pertemuan1/latihan15Logika/src/latihan15logika/Latihan15Logika.java
@@ -0,0 +1,27 @@
+/*
+ * nama : yongki agustin
+ * kelas : if6k
+ * nim : 10117903
+ * Todo : logika v2
+ */
+package latihan15logika;
+
+/**
+ *
+ * @author Teknisi
+ */
+public class Latihan15Logika {
+
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b)) = " + !(a && b));
+
+
+    }
+}
