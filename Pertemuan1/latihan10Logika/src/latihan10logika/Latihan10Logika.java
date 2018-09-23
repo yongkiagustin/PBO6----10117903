78ed6261e17aba50e888c89695a17e4fd06f1ce2
yongkiagustin <yongkiagustin92@gmail.com>
2018-09-23 15:54:21 +0700

    mencoba

---
+++ Pertemuan1/latihan10Logika/src/latihan10logika/Latihan10Logika.java
@@ -0,0 +1,22 @@
+/*
+ * nama : yongki agustin
+ * kelas : if6k
+ * nim : 10117903
+ * Todo : latihan logika
+ */
+package latihan10logika;
+
+
+public class Latihan10Logika {
+
+    public static void main(String[] args) {
+
+        int age = 19;
+        boolean mature;
+
+        mature = (age >= 17);
+
+        System.out.println("Status Dewasa: " + mature);
+
+    }
+}
