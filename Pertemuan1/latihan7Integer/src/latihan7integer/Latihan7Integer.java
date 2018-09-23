9df9b18ddb324cd1a886c9492171cb40808f145a
yongkiagustin <yongkiagustin92@gmail.com>
2018-09-23 15:47:50 +0700

    mencoba

---
+++ Pertemuan1/latihan7Integer/src/latihan7integer/Latihan7Integer.java
@@ -0,0 +1,34 @@
+/*
+ * nama : yongki agustin
+ * kelas : if6k
+ * nim : 10117903
+ * Todo : integer   
+ */
+package latihan7integer;
+
+/**
+ *
+ * @author Teknisi
+ */
+public class Latihan7Integer {
+
+    public static void main(String[] args) {
+
+        byte vByte;
+        short vShort;
+        int vInt;
+        long vLong;
+
+        vByte = 120;
+        vShort = 12059;
+        vInt = 1_000_000_000;
+        vLong = 900000000000000L;
+
+        System.out.printf("Nilai byte : %d\r\n", vByte);
+        System.out.printf("Nilai short : %d\r\n", vShort);
+        System.out.printf("Nilai int : %d\r\n", vInt);
+        System.out.printf("Nilai long : %d\r\n", vLong);
+
+    }
+
+}
