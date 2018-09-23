1c90a29d755bdc0ce78194d72e8fe2184861acaf
yongkiagustin <yongkiagustin92@gmail.com>
2018-09-23 15:52:16 +0700

    mencoba

---
+++ Pertemuan1/latihan9Teks/src/latihan9teks/Latihan9Teks.java
@@ -0,0 +1,24 @@
+/*
+ * nama : yongki agustin
+ * kelas : if6k
+ * nim : 10117903
+ * Todo : latihan teks 
+ */
+package latihan9teks;
+
+
+public class Latihan9Teks {
+
+    public static void main(String[] args) {
+
+        char vChar;
+        char tab = '\t';
+        char newline = '\n';
+
+        vChar = 'A';
+
+        System.out.println("Hasil" + newline + "Char : " + tab + "berisi " + vChar);
+
+    }
+
+}
