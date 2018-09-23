13626bacd33ee8079876094d59c6d9b0068afab7
yongkiagustin <yongkiagustin92@gmail.com>
2018-09-23 15:38:16 +0700

    mencoba

---
+++ Pertemuan1/latihan4/src/latihan4/Latihan4.java
@@ -0,0 +1,26 @@
+/*
+ * nama : yongki agustin
+ * kelas : if6k
+ * nim : 10117903
+ * Todo : latihan kedua   
+ */
+package latihan4;
+
+public class Latihan4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void tambahKambing(){
+        //deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing +5;
+        System.out.println("Jumlah Kambing setelah ditambah : 0" +jumlahKambing);
+    }
+    public static void main(String[] args) {
+        Latihan4 kambingJantan = new Latihan4();
+        kambingJantan.tambahKambing();
+         
+    }
+}
