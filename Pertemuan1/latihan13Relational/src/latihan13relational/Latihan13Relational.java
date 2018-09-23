6a61ce4be6445f2dbbef7658870effa6f4c1d1e3
yongkiagustin <yongkiagustin92@gmail.com>
2018-09-23 16:01:21 +0700

    mencoba

---
+++ Pertemuan1/latihan13Relational/src/latihan13relational/Latihan13Relational.java
@@ -0,0 +1,26 @@
+/*
+ * nama : yongki agustin
+ * kelas : if6k
+ * nim : 10117903
+ * Todo : relational 
+ */
+package latihan13relational;
+
+
+public class Latihan13Relational {
+
+    public static void main(String[] args) {
+
+        int A = 10;
+        int B = 20;
+
+        System.out.println("a == b = " + (A == B));
+        System.out.println("a != b = " + (A != B));
+        System.out.println("a > b = " + (A > B));
+        System.out.println("a < b = " + (A < B));
+        System.out.println("b >= a = " + (B >= A));
+        System.out.println("b <= a = " + (B <= A));
+
+    }
+
+}
