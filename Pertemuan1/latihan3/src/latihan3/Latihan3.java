781ae518fdae24da7c08e2d7b1d317cbcbe39828
yongkiagustin <yongkiagustin92@gmail.com>
2018-09-23 15:38:09 +0700

    mencoba

---
+++ Pertemuan1/latihan3/src/latihan3/Latihan3.java
@@ -0,0 +1,25 @@
+/*
+ * nama : yongki agustin
+ * kelas : if6k
+ * nim : 10117903
+ * Todo : latihan kedua   
+ */
+
+package latihan3;
+import java.util.Scanner;
+
+public class Latihan3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukan Nama anda\t: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Nama anda adalah\t: " + name);
+    }
+
+}
